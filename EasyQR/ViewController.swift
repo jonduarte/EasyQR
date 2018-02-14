83c447a66b72b362c76bb666eef6408d2e08b280
Jonathan Duarte <jonathan.duarte@rocketmail.com>
2018-02-14 23:46:30 +0100

    Initial commit

--- EasyQR/ViewController.swift
+++ EasyQR/ViewController.swift
@@ -7,12 +7,53 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
 
+    var video = AVCaptureVideoPreviewLayer()
+    
+    @IBOutlet var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let session = AVCaptureSession()
+        let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            session.addInput(input)
+        } catch {
+            print("ERROR")
+        }
+        
+        let output = AVCaptureMetadataOutput()
+        session.addOutput(output)
+        
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+    
+        output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+        video = AVCaptureVideoPreviewLayer(session: session)
+        video.frame = view.layer.bounds
+        view.layer.addSublayer(video)
+        self.view.bringSubview(toFront: image)
+        session.startRunning()
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+    
+        if metadataObjects.count > 0 {
+            let object = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+            if object.type == AVMetadataObject.ObjectType.qr {
+                let alert = UIAlertController(title: "QR Code", message: object.stringValue, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Retake", style: .default, handler: nil))
+                alert.addAction(UIAlertAction(title: "Copy", style: .default, handler: { (nil) in
+                    UIPasteboard.general.string = object.stringValue
+                }))
+                present(alert, animated: true, completion: nil)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Pods/Target Support Files/Pods-EasyQR/Pods-EasyQR-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EasyQR : NSObject
+@end
+@implementation PodsDummy_Pods_EasyQR
+@end
